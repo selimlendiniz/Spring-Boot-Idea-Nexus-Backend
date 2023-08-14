f93a4b73f04e0d462cc74b41eaebd8fe11a022c6
Selim Arda Cevik <sardacevik@icloud.com>
2023-08-14 13:36:20 +0300

    initial commit

---
+++ src/main/java/com/ideanexus/ideanexusbackend/IdeaNexusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ideanexus.ideanexusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaNexusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaNexusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideanexus/ideanexusbackend/IdeaNexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideanexus.ideanexusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaNexusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
